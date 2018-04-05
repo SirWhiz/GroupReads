9187724f9afa8b7e78e2533380a7eb70d08b85e3
Alvaro Ferrera <applewhiz@outlook.es>
2018-04-05 12:07:28 +0100

    Added front page typing animation using Typed.js library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'GroupReads';
+
+  ngOnInit(){
+    var typed = new Typed(".auto", {
+      strings: ["lectores^2500", "aventureros^2500", "amantes^2500",],
+      typeSpeed:60,
+      fadeOut: true,
+      loop:true      
+    });
+  }
 }
