62e91120ea80545372e6da804408fa62b2d53483
Alvaro Ferrera <applewhiz@outlook.es>
2018-04-17 11:01:22 +0100

    Fixed page scroll on page change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public title = "GroupReads";
+
+	public title:string = "";
+	constructor(private router: Router){
+		this.title = "GroupReads";
+	}
+
+	ngOnInit(){
+		this.router.events.subscribe((evt) => {
+			if(!(evt instanceof NavigationEnd)){
+				return;
+			}
+			window.scrollTo(0,0)
+		});
+	}
 }
