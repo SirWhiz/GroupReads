1560ac9aa303c1ded70bfc60c6cb41def71ef9dd
Alvaro Ferrera <applewhiz@outlook.es>
2018-06-11 11:13:59 +0100

    Trying to fix cors

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ var server = app.listen(8080);
 var io = require('socket.io').listen(server);
 
 // Use the /dist directory
-app.use(express.static(__dirname + '/dist'));
+//app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist'),function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // Catch all other invalid routes
 app.all('*', function(req,res){
